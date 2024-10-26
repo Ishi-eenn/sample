04b0d488bc60d1915caf86e5b7cc8ebcdb62dcc6
Ishi <syi378039@gmail.com>
2024-10-26 18:28:20 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import NovelPage from "./pages/NovelPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <a
+      {/* <a
         href="https://bungei-zorozoro.com/2/1729169756/c"
         className="btn btn-svg"
       >
@@ -17,7 +18,8 @@ function App() {
           <rect x="2" y="2" rx="0" width="200" height="50" fill="none" />
         </svg>
         <span>PUSH！</span>
-      </a>
+      </a> */}
+      <NovelPage />
     </>
   );
 }
---
+++ src/pages/NovelPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  minHeight: "100vh",
+  width: "100%",
+  padding: "20px",
+};
+
+const verticalTextStyle = {
+  writingMode: "vertical-rl",
+  textOrientation: "upright",
+  whiteSpace: "pre-wrap",
+  lineHeight: "2",
+  maxHeight: "calc(100vh - 40px)",
+  overflowY: "auto",
+  margin: "0 auto 0 0",
+  textAlign: "left",
+  fontSize: "1.5rem",
+  fontWeight: "400",
+  letterSpacing: "0.05em",
+};
+
+const NovelPage = () => {
+  const [textContent, setTextContent] = useState("");
+
+  useEffect(() => {
+    fetch("/message.txt")
+      .then((response) => response.text())
+      .then((data) => setTextContent(data))
+      .catch((error) => console.error("Error loading text file:", error));
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={verticalTextStyle}>
+        {textContent.split("<br/>").map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NovelPage;
