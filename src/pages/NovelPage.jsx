2fbaf6aa2c5d1f334a0bec0ea192290e908e0016
Ishi <syi378039@gmail.com>
2024-10-26 19:16:08 +0900

    test

--- src/pages/NovelPage.jsx
+++ src/pages/NovelPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 const containerStyle = {
-  display: "flex",
   justifyContent: "center", // 中央寄せ
   alignItems: "flex-start",
   minHeight: "100vh",
